8cb0c95ff7c2e3651d5ba12cefc6e7be2be355ef
ejais58 <ejais58@gmail.com>
2022-01-16 17:48:26 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Contador app';
-  numero: number = 10;
-
-  sumar(){
-    this.numero += 1;
-  };
-
-  restar(){
-    this.numero -= 1;
-  };
+  
 }
---
+++ src/app/heroes/heroe/heroe.component.ts
@@ -0,0 +1,27 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: "app-heroe",
+    templateUrl: "heroe.component.html"
+})
+export class HeroeComponent{
+    nombre: string = 'Ironman';
+    edad: number = 35;
+
+    get nombreCapitalizado(): String{
+        return this.nombre.toUpperCase();
+    };
+
+    obtenerNombre() : String {
+        return `${this.nombre} - ${this.edad}`;
+    }
+
+    cambiarNombre(): void {
+        this.nombre = 'Spiderman';
+    }
+
+    cambiarEdad(): void {
+        this.edad = 30;
+    }
+}
